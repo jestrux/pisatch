c773b6761b113fa98c3028e05b3ccd0349fb569e
Walter Kimaro <wakyj07@gmail.com>
2019-10-06 16:34:54 +0300

    Clean up UI, add copying images, add tweet about app

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const { menubar } = require('menubar');
+const { ipcMain, clipboard, shell } = require("electron");
 
 const mb = menubar({
-  width: 600, height: 400,
+  icon: './icon.png',
+  browserWindow: {
+    width: 630, height: 80,
+  },
+  background: "#000000",
+  preloadWindow: true,
   webPreferences: {nodeIntegration: true}
 });
 
 mb.on('ready', () => {
   console.log('app is ready');
 });
+
+ipcMain.on('send-tweet', () => {
+  let twitterUrl = "https://twitter.com/intent/tweet?text=";
+  twitterUrl += "Yo, check out this really cool image search menu app by @jestrux it's 🔥🔥";
+  twitterUrl += " &url=https://github.com/jestrux/Search-Images-Menu-App";
+
+  shell.openExternal(twitterUrl);
+});
+
+ipcMain.on('copy-link', (event, link) => {
+  clipboard.writeText(link, 'selection')
+});
+
+ipcMain.on('state-change', (event, state) => {
+  switch (state) {
+    case "searching":
+    case "no-results":
+      mb.window.setSize(630, 150)
+      break;
+  
+    default:
+      mb.window.setSize(630, 460);
+      break;
+  }
+})
 
 // mb.on('after-create-window', () => mb.window.openDevTools())
